ea8edf57bd9c4f1120ff42abd923f5025ecb605f
Viacheslav <wooofah@gmail.com>
2019-11-20 15:29:25 +0200

    fetch data with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [names, setNames] = useState([]);
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character/1')
+      .then(res => res.json())
+      .then(data => {
+        setNames(data)
+      })
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {names.name}
     </div>
-  );
+  )
+
 }
 
 export default App;
