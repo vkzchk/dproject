8f6a896289042f9542463b89b011068e0b8456c9
Viacheslav <wooofah@gmail.com>
2019-11-22 11:14:51 +0200

    TypeError: persons.map is not a function

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -3,20 +3,26 @@ import React, { useState, useEffect } from 'react';
 import './content.css';
 
 const Content = () => {
-  const [data, setData] = useState([]);
+  const [persons, setPersons] = useState([]);
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/')
       .then(res => res.json())
-      .then(data => {
-        setData(
-          JSON.stringify(data)
-        )
-      })
+      .then(data => { setPersons(data) })
   }, []);
   return (
     <div className="content">
       <div className="items">
-        <span>{data}</span>
+        {
+          persons.map(person => (
+            <div>
+              <div>{person.image}</div>
+              <div>{person.name}</div>
+              <div>{person.status}</div>
+              <div>{person.species}</div>
+              <div>{person.gender}</div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
