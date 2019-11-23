ed600ffc17124623eb5a2844e8d8325f1f277eea
Viacheslav <wooofah@gmail.com>
2019-11-23 15:04:31 +0200

    change page buttons added

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -3,14 +3,17 @@ import React, { useState, useEffect } from 'react';
 import './content.css';
 
 const Content = () => {
+  const [pageIndex, setPageIndex] = useState(1)
   const [persons, setPersons] = useState([]);
   useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character/')
+    fetch(`https://rickandmortyapi.com/api/character/?page=${pageIndex}`)
       .then(res => res.json())
       .then(data => { setPersons(data.results) })
-  }, []);
+  }, [pageIndex]);
   return (
     <div className="content">
+      <button type="button" onClick={() => pageIndex > 1 ? setPageIndex(pageIndex - 1) : 1}>Prev Page</button>
+      <button type="button" onClick={() => pageIndex < 25 ? setPageIndex(pageIndex + 1) : 25}>Next Page</button>
       <div className="items">
         {
           persons.map(person => (
