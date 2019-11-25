e25a5c7b1208bd2d4c870e3d27317ee001cabb36
Viacheslav <wooofah@gmail.com>
2019-11-25 17:55:58 +0200

    added contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './contact.css';
 
 const Contact = () => {
-    return (
-        <div className="contact">
-            <h1>Contact page</h1>
-        </div>
-    )
+  const [name, setName] = useState('')
+  const [inputText, setInputText] = useState('')
+  const [email, setEmail] = useState('')
+  return (
+    <div className="contact">
+      <p>Contact me</p>
+      <form className="contact-form">
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name.."
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Your email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <br />
+        <label htmlFor="text">Text: </label>
+        <textarea
+          id="text"
+          placeholder="Write something.."
+          value={inputText}
+          onChange={e => setInputText(e.target.value)}
+        />
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  )
 }
 
 export default Contact;
