4d9a66f01ea0f895e6670f143dfaae19760dfaef
Viacheslav <wooofah@gmail.com>
2019-11-28 13:51:18 +0200

    paginator added

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,29 +1,40 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import Paginator from 'react-hooks-paginator';
 
 import './portfolio.css';
 import Navprojects from '../navprojects/navprojects'
 
 const Portfolio = () => {
+
   const [currentPage, setCurrentPage] = useState(1);
   const [persons, setPersons] = useState([]);
-  const [pageLimit, setPageLimit] = useState(1);
+  const [pages, setPages] = useState(1);
+
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`)
       .then(res => res.json())
       .then(data => {
         setPersons(data.results)
-        setPageLimit(data.info.pages)
+        setPages(data.info.pages)
       })
   }, [currentPage]);
 
-  const pageNumber = new Array(pageLimit).fill(null).map((it, index) => index + 1)
+  const pageNumbers = new Array(pages).fill(null).map((it, index) => index + 1)
+
   return (
     <Fragment>
       <Navprojects />
       <div className="portfolio">
-        <div className="paginate">
-          {pageNumber.map(page => <span onClick={() => setCurrentPage(page)}>{page}</span>)}
-        </div>
+        <Paginator
+          totalRecords={pageNumbers.length}
+          pageLimit={pages}
+          pageNeighbours={2}
+          pagePrevText='« Prev'
+          setOffset={() => { }}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          pageNextText='Next »'
+        />
         <div className="items">
           {
             persons.map(person => (
